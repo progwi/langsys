28caa2a9bb7f1797cb6ec415cebe56ccbc724e8c
Emilio Ormeño <eormeno@gmail.com>
2022-08-24 17:50:34 -0300

    read initial json file

--- src/test.php
+++ src/test.php
@@ -4,16 +4,17 @@ require_once __DIR__ . '/models/User.php';
 require_once __DIR__ . '/models/Role.php';
 require_once __DIR__ . '/models/Person.php';
 
-$role0 = new Role(0, 'guest');
-$role1 = new Role(1, 'student');
-$role2 = new Role(2, 'professor');
-$role3 = new Role(3, 'admin');
-$role4 = new Role(4, 'developer');
+$roles = [];
+$initialRoles = json_decode(file_get_contents(__DIR__ . '/models/roles.json'), true);
+
+foreach ($initialRoles as $role) {
+	$roles[] = new Role($role['id'], $role['name']);
+}
 
 $usuario2 = new User('mscheffer', 'mscheffer@gmail.com', '123456');
 //$usuario2->addRole($role1);
-$usuario2->addRole($role2);
-//$usuario2->addRole($role4);
+$usuario2->addRole($roles[2]);
+$usuario2->addRole($roles[3]);
 
 $maru = new Person('Maru', 'Scheffer', $usuario2);
 
